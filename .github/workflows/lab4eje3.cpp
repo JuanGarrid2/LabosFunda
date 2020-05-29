7b8c5aee27449cdfef129573b65de574315a8d76
JuanGarrid2 <62636772+JuanGarrid2@users.noreply.github.com>
2020-05-28 21:03:05 -0600

    lab4

---
+++ .github/workflows/lab4eje3.cpp
@@ -0,0 +1,29 @@
+#include "iostream"
+	using namespace std;
+	
+	int main(){
+	
+	    int a;
+	
+	    cout << "Digite un numero"<<endl;
+	    cin >> a;
+	
+	    if(a == 0){
+	
+	        cout << a << " es igual a cero" << endl;
+	
+	
+	    }else if(a>0){
+	        
+	        cout << a << " es una numero positivo" << endl;
+	
+	        
+	        
+	    }else{
+	        cout << a << " es un numero negativo" << endl;
+	
+	        
+	    }
+	
+	    return 0;
+	}
